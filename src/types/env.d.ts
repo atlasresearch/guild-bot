d992ef686945242626d9542d55ed65fa4aada670
HexaField <10372036+HexaField@users.noreply.github.com>
2026-01-17 22:04:05 +1100

    clean up

--- src/types/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    DISCORD_TOKEN: string
-    LLM_URL: string
-    LLM_API_KEY?: string
-    RECORDING_TRANSCRIPT_CHANNEL_ID?: string
-    CHANNEL_ID?: string
-  }
-}
